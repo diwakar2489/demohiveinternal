269ed6c44cabada07f240599c11f915e4af3a29b
Diwakar Pandey <diwakarnice@rediffmail.com>
2022-09-14 14:48:48 +0530

    project list method change

--- src/routes/project.js
+++ src/routes/project.js
@@ -4,7 +4,7 @@ const ProjectCtrl = require('../controllers/project/project_api');
 
 // const validater = require("../middlewere/vailidation");
 
-router.post('/project',ProjectCtrl.List);
+router.get('/project',ProjectCtrl.List);
 router.post('/addproject',ProjectCtrl.add);
 router.post('/edit',ProjectCtrl.edit);
 router.post('/update',ProjectCtrl.update);
